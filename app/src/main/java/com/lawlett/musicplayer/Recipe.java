87f220f6b89fe889390d7739a776191adc7d29a9
Sotaire <islamov-tilek@mail.ru>
2020-09-23 02:19:24 +0600

    Initial commit

---
+++ app/src/main/java/com/lawlett/musicplayer/Recipe.java
@@ -0,0 +1,13 @@
+package com.lawlett.musicplayer;
+
+public class Recipe {
+    private String mLinkImage;
+    private String mNameRecipe;
+    private String mLinkPageRecipe;
+
+    public Recipe(String mLinkImage, String mNameRecipe, String mLinkPageRecipe) {
+        this.mLinkImage = mLinkImage;
+        this.mNameRecipe = mNameRecipe;
+        this.mLinkPageRecipe = mLinkPageRecipe;
+    }
+}
